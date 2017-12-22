08ff1ad30d8fc3a1dde0d53d66c90672bb4a5818
alexzicat <dev.alexzicat@gmail.com>
2017-12-21 23:58:46 -0500

    add flatten function

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ const fs = require('fs');
 const glob = require('glob');
 const yaml = require('js-yaml');
 
+const flatten = (locales) {
+  const data = {};
+
+  Object.keys(locales).forEach((key) => {
+    const value = locales[key];
+
+    if (typeof value === 'object') {
+      Object.keys(flatten(value)).forEach((childKey) => {
+        data[`${key}.${childKey}`] = value[childKey];
+      });
+    } else {
+      data[key] = value;
+    }
+  });
+
+  return data;
+};
+
 module.exports = (input, options) => {
   if (!input) {
     console.log('Error ! Please specify an input directory.');
@@ -25,7 +43,7 @@ module.exports = (input, options) => {
     files.forEach((file) => {
       const locales = yaml.safeLoad(fs.readFileSync(file, 'utf8'), {json: true});
 
-      console.log(locales);
+      console.log(flatten(locales));
     });
   });
 };
